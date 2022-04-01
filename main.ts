148dcdc077e7fa9b9f4fe806120a561b2cb72a54
omarturmun <93262369+omarturmun@users.noreply.github.com>
2022-04-01 14:23:40 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ basic.forever(function () {
 })
 basic.forever(function () {
     birak = parseFloat(DFRobotMaqueenPlus.readeDistance(Motors1.M1))
-    if (birak < 7) {
+    if (birak < 26.15) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
     } else {
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
